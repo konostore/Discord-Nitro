5765e5a6dd9a94a1f98bd9d53e2eb18b7438677b
Zurry <39099474+Zurry@users.noreply.github.com>
2023-03-09 14:35:30 -0800

    Add Firmware Source Files

---
+++ Discord Nitro Firmware Source/keymaps/idobao/rules.mk
@@ -0,0 +1,5 @@
+# Copyright 2022 Vino Rodrigues (@vinorodrigues)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+LTO_ENABLE = yes
+VIA_ENABLE = yes
---
+++ Discord Nitro Firmware Source/rules.mk
@@ -0,0 +1,5 @@
+# Copyright 2022 Vino Rodrigues (@vinorodrigues)
+# SPDX-License-Identifier: GPL-30-or-later
+
+RGB_MATRIX_ENABLE = yes     # Enable RGB Matrix feature
+RGB_MATRIX_DRIVER = WS2812 	# ID67 uses WS2812 driver
